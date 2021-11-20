853fd94c64db3dd281a0b94f46dd0772abc38e28
lddang1762 <lddang1762@csu.fullerton.edu>
2021-11-20 14:39:29 -0800

    Completed HW6

---
+++ app/scripts/src/dom.js
@@ -0,0 +1,129 @@
+let $ = require("jquery");
+let md5 = require("crypto-js/md5");
+let moment = require("moment");
+
+function createGravatarUrl(username) {
+  let userhash = md5(username);
+  userhash = userhash.toString();
+  return `http://www.gravatar.com/avatar/${userhash}`;
+}
+
+export function promptForUsername() {
+  let username = prompt("Enter a username");
+  return username.toLowerCase();
+}
+
+export class ChatForm {
+  constructor(formId, inputId) {
+    this.formId = formId;
+    this.inputId = inputId;
+  }
+  init(submitCallback) {
+    $(this.formId).submit((event) => {
+      console.log(event);
+      event.preventDefault();
+      let val = $(this.inputId).val();
+      submitCallback(val);
+      $(this.inputId).val("");
+    });
+    $(this.formId)
+      .find("button")
+      .on("click", () => {
+        $(this.formId).submit();
+      });
+  }
+}
+
+export class ChatList {
+  constructor(listId, username) {
+    this.listId = listId;
+    this.odd = false;
+    this.username = username;
+  }
+
+  init() {
+    this.timer = setInterval(() => {
+      $("[data-time]").each((idx, element) => {
+        let $element = $(element);
+        let timestamp = new Date().setTime($element.attr("data-time"));
+        let ago = moment(timestamp).fromNow();
+        $element.html(ago);
+      });
+    }, 1000);
+  }
+
+  drawMessage(messageData) {
+    var $messageRow = $("<li>", {
+      class: "message-row",
+    });
+
+    var $img = $("<img>", {
+      class: "img-other",
+      src: createGravatarUrl(messageData.user),
+      title: messageData.user,
+    });
+    $messageRow.append($img);
+
+    var $message = $("<p>");
+    $message.append(
+      $("<span>", {
+        class: "message-username",
+        text: messageData.user,
+      })
+    );
+    $message.append(
+      $("<span>", {
+        class: "timestamp",
+        "data-time": messageData.timestamp,
+        text: moment(messageData.timestamp).fromNow(),
+      })
+    );
+    $message.append(
+      $("<span>", {
+        class: "message-message",
+        text: messageData.message,
+      })
+    );
+    $messageRow.append($message);
+
+    $(this.listId).append($messageRow);
+    $messageRow.get(0).scrollIntoView();
+  }
+
+  drawMyMessage(messageData) {
+    var $messageRow = $("<li>", {
+      class: "message-row me",
+    });
+
+    var $img = $("<img>", {
+      src: createGravatarUrl(messageData.user),
+      title: messageData.user,
+    });
+    $messageRow.append($img);
+
+    var $message = $("<p>");
+    $message.append(
+      $("<span>", {
+        class: "message-username",
+        text: messageData.user,
+      })
+    );
+    $message.append(
+      $("<span>", {
+        class: "timestamp",
+        "data-time": messageData.timestamp,
+        text: moment(messageData.timestamp).fromNow(),
+      })
+    );
+    $message.append(
+      $("<span>", {
+        class: "message-message",
+        text: messageData.message,
+      })
+    );
+    $messageRow.append($message);
+
+    $(this.listId).append($messageRow);
+    $messageRow.get(0).scrollIntoView();
+  }
+}
